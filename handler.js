9aeeae795f7d850b4e0b9fb3604a6304b7ee21c2
Ayush Sharma <66960144+tsayush@users.noreply.github.com>
2020-08-04 18:05:41 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello World.11',
+        message: 'Hello World.Feat1',
         input: event,
       },
       null,
